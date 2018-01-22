ea0d02526947a26ec0c04185d73e0fe6b5d5458e
nhwltrs <nhwltrs>
2018-01-22 18:20:53 -0500

    Fixed Teams activity

--- app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/NewTeam.java
+++ app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/NewTeam.java
@@ -61,10 +61,10 @@ public class NewTeam extends AppCompatActivity {
                     return;
                 }
 
-                //Intent teams = new Intent(getApplicationContext(), Teams.class);
-                //teams.putExtra("Team Name", checkTeamName);
-                //teams.putExtra("Team Number", checkTeamNumber);
-                //startActivity(teams);
+                Intent teams = new Intent(getApplicationContext(), Teams.class);
+                teams.putExtra("Team Name",checkTeamName);
+                teams.putExtra("Team Number", checkTeamNumber);
+                startActivity(teams);
             }
         });
     }
--- app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/Teams.java
+++ app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/Teams.java
@@ -4,11 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.salinesingularity.nhwltrs.salinesingularity2018scoutingapp.MatchInformation;
+import com.salinesingularity.nhwltrs.salinesingularity2018scoutingapp.NewTeam;
+import com.salinesingularity.nhwltrs.salinesingularity2018scoutingapp.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Teams extends AppCompatActivity {
 
@@ -17,16 +27,27 @@ public class Teams extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teams);
 
-        Button newTeam = (Button)findViewById(R.id.newTeamButton);
-        ListView list = (ListView)findViewById(R.id.teamsListView);
-        Bundle extras = getIntent().getExtras();
+        Button newTeam = (Button)findViewById(R.id.newTeamsButton);
+        ListView list = (ListView) findViewById(R.id.teamsListView);
 
-        final ArrayList<String> arrayList = new ArrayList<String>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
-        list.setAdapter(arrayAdapter);
+        List<HashMap<String, String>> listItems = new ArrayList<>();
+        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.list_item, new String[]{"First Line", "Second Line"}, new int[]{R.id.teamNameTextView, R.id.teamNumberTextView});
+        HashMap<String, String> resultsMap = new HashMap<>();
+        list.setAdapter(adapter);
+        if (getIntent().hasExtra("Team Name")) {
+            resultsMap.put("First Line", getIntent().getExtras().getString("Team Name"));
+            resultsMap.put("Second Line", getIntent().getExtras().getString("Team Number"));
+            listItems.add(resultsMap);
+            adapter.notifyDataSetChanged();
+        }
 
-        //arrayList.add(extras.getString("Team Name"));
-        //arrayAdapter.notifyDataSetChanged();
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent matchInformation = new Intent (getApplicationContext(), MatchInformation.class);
+                startActivity(matchInformation);
+            }
+        });
 
         newTeam.setOnClickListener(new View.OnClickListener() {
             @Override
