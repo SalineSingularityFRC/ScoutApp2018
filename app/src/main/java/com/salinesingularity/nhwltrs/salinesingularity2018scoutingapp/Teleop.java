dc65db59fb2f139b336cc2d1d2c4be6088146560
nhwltrs <nhwltrs>
2018-01-22 20:17:40 -0500

    Created the Vault and Portal counters in Teleop

--- app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/Teleop.java
+++ app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/Teleop.java
@@ -14,6 +14,9 @@ public class Teleop extends Fragment {
     private static final String TAG = "Teleop";
     int allianceSwitchCounterLevel = 0;
     int scaleCounterLevel = 0;
+    int opponentsSwitchCounterLevel = 0;
+    int portalCounterLevel = 0;
+    int vaultCounterLevel = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +28,15 @@ public class Teleop extends Fragment {
         Button scaleMinus = (Button) view.findViewById(R.id.teleopScaleMinusButton);
         Button scaleAdd = (Button) view.findViewById(R.id.teleopScaleAddButton);
         final TextView scaleCounter = (TextView) view.findViewById(R.id.teleopScaleCounterTextView);
+        Button opponentSwitchMinus = (Button) view.findViewById(R.id.teleopOpponentsSwitchMinusButton);
+        Button opponentSwitchAdd = (Button) view.findViewById(R.id.teleopOpponentsSwitchAddButton);
+        final TextView opponentSwitchCounter = (TextView) view.findViewById(R.id.teleopOpponentsSwitchCounterTextView);
+        Button portalMinus = (Button) view.findViewById(R.id.teleopPortalMinusButton);
+        Button portalAdd = (Button) view.findViewById(R.id.teleopPortalAddButton);
+        final TextView portalCounter = (TextView) view.findViewById(R.id.teleopPortalCounterTextView);
+        Button vaultMinus = (Button) view.findViewById(R.id.teleopVaultMinusButton);
+        Button vaultAdd = (Button) view.findViewById(R.id.teleopVaultAddButton);
+        final TextView vaultCounter = (TextView) view.findViewById(R.id.teleopVaultCounterTextView);
 
         allianceSwitchMinus.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +70,60 @@ public class Teleop extends Fragment {
                 if (scaleCounterLevel> 0) {
                     scaleCounterLevel--;
                     scaleCounter.setText(scaleCounterLevel + "");
+                }
+            }
+        });
+
+        opponentSwitchMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (opponentsSwitchCounterLevel > 0) {
+                    opponentsSwitchCounterLevel--;
+                    opponentSwitchCounter.setText(opponentsSwitchCounterLevel + "");
+                }
+            }
+        });
+
+        opponentSwitchAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opponentsSwitchCounterLevel++;
+                opponentSwitchCounter.setText(opponentsSwitchCounterLevel + "");
+            }
+        });
+
+        portalAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                portalCounterLevel++;
+                portalCounter.setText(portalCounterLevel + "");
+            }
+        });
+
+        portalMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (portalCounterLevel > 0) {
+                    portalCounterLevel--;
+                    portalCounter.setText(portalCounterLevel + "");
+                }
+            }
+        });
+
+        vaultAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vaultCounterLevel++;
+                vaultCounter.setText(vaultCounterLevel + "");
+            }
+        });
+
+        vaultMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (vaultCounterLevel > 0) {
+                    vaultCounterLevel--;
+                    vaultCounter.setText(vaultCounterLevel + "");
                 }
             }
         });
