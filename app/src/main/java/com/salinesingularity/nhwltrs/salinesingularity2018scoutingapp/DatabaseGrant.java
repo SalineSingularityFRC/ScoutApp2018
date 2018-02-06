5eead138a91d0bbf1c010a40a13c26ce43d3866c
Grant <gringrant@gmail.com>
2018-02-05 19:45:48 -0500

    Database now handles JSON errors.

--- app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/DatabaseGrant.java
+++ app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/DatabaseGrant.java
@@ -45,33 +45,57 @@ public class DatabaseGrant {
         }
     }
 
-    public static void makeTeam(int teamNumber, String teamName) throws JSONException{
-        tempTeamData.put(new JSONObject("{"+
-                "\"type\":\"team\"," +
-                "\"team\":" + teamNumber + "," +
-                "\"name\":\"" + teamName + "\"" +
-        "}"));
-        send();
+    public static void makeTeam(int teamNumber, String teamName){
+        try {
+            tempTeamData.put(new JSONObject("{"+
+                    "\"type\":\"team\"," +
+                    "\"team\":" + teamNumber + "," +
+                    "\"name\":\"" + teamName + "\"" +
+            "}"));
+            send();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static int getTeamNumber(int index) throws JSONException {
-        return teamData.getJSONObject(index).getInt("team");
+    public static int getTeamNumber(int index){
+        try {
+            return teamData.getJSONObject(index).getInt("team");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
-    public static String getTeamName(int index) throws JSONException {
-        return teamData.getJSONObject(index).getString("name");
+    public static String getTeamName(int index) {
+        try {
+            return teamData.getJSONObject(index).getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "Error";
     }
 
     public static int getTeamDatabaseLength(){
         return teamData.length();
     }
 
-    public static int getLocalTeamNumber(int index) throws JSONException {
-        return tempTeamData.getJSONObject(index).getInt("team");
+    public static int getLocalTeamNumber(int index) {
+        try {
+            return tempTeamData.getJSONObject(index).getInt("team");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
-    public static String getLocalTeamName(int index) throws JSONException {
-        return tempTeamData.getJSONObject(index).getString("name");
+    public static String getLocalTeamName(int index) {
+        try {
+            return tempTeamData.getJSONObject(index).getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return "Error";
     }
 
     public static int getLocalTeamDatabaseLength(){
@@ -97,74 +121,130 @@ public class DatabaseGrant {
         bluetooth.send("{\"matchData\":"+ robotMatchData.toString()+",\"teamData\":"+tempTeamData.toString()+"}");
     }
 
-    public static void createRobotMatch(int teamNumber, String match, boolean onBlue) throws JSONException{
-        tempRobotMatchData = new JSONObject("{" +
-                "\"type\":\"robotMatch\"," +
-                "\"team\":" + teamNumber + "," +
-                "\"matchID\":\"" + match + "\"," +
-                "\"onBlue\":"+onBlue+"," +
-                "\"scale\":[]," +
-                "\"switchFriendly\":[]," +
-                "\"switchEnemy\":[]," +
-                "\"vault\":[]," +
-                "\"portal\":[]," +
-                "\"startingPos\":-1," + //-1 error, 0-2 close-far
-                "\"autonSkill\":-1," + //-1 error, 0 No auton, 1 Passes Base line, (Wrong side: -)2 Switch, (Wrong side: -)3 Scale, 5 Robot eats power cube
-                "\"climbSkill\":-1" + //-1 error, 0 Not in contact with BASE, 1 In contact with BASE, 2 Completed CLIMB
-                "}");
+    public static void createRobotMatch(int teamNumber, String match, boolean onBlue){
+        try {
+            tempRobotMatchData = new JSONObject("{" +
+                    "\"type\":\"robotMatch\"," +
+                    "\"team\":" + teamNumber + "," +
+                    "\"matchID\":\"" + match + "\"," +
+                    "\"onBlue\":"+onBlue+"," +
+                    "\"scale\":[]," +
+                    "\"switchFriendly\":[]," +
+                    "\"switchEnemy\":[]," +
+                    "\"vault\":[]," +
+                    "\"portal\":[]," +
+                    "\"startingPos\":-1," + //-1 error, 0-2 close-far
+                    "\"autonSkill\":-1," + //-1 error, 0 No auton, 1 Passes Base line, (Wrong side: -)2 Switch, (Wrong side: -)3 Scale, 5 Robot eats power cube
+                    "\"climbSkill\":-1" + //-1 error, 0 Not in contact with BASE, 1 In contact with BASE, 2 Completed CLIMB
+                    "}");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         // Meta data goes here
     }
 
-    public static void setStartingPos(int pos) throws JSONException { // 0-2 close-far
-        tempRobotMatchData.put("startingPos",pos);
+    public static void setStartingPos(int pos){ // 0-2 close-far
+        try {
+            tempRobotMatchData.put("startingPos",pos);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void setAutonSkill(int skill) throws JSONException { // 0 No auton, 1 Passes Base line, (Wrong side: -)2 Switch, (Wrong side: -)3 Scale, 5 Robot eats power cube
-        tempRobotMatchData.put("autonSkill",skill);
+    public static void setAutonSkill(int skill){ // 0 No auton, 1 Passes Base line, (Wrong side: -)2 Switch, (Wrong side: -)3 Scale, 5 Robot eats power cube
+        try {
+            tempRobotMatchData.put("autonSkill",skill);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void setClimbSkill(int skill) throws JSONException { // 0 Not in contact with BASE, 1 In contact with BASE, 2 Completed CLIMB
-        tempRobotMatchData.put("climbSkill",skill);
+    public static void setClimbSkill(int skill){ // 0 Not in contact with BASE, 1 In contact with BASE, 2 Completed CLIMB
+        try {
+            tempRobotMatchData.put("climbSkill",skill);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addScale(String type, int ms) throws JSONException {
-        tempRobotMatchData.getJSONArray("scale").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+    public static void addScale(String type, int ms){
+        try {
+            tempRobotMatchData.getJSONArray("scale").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addSwitchFriendly(String type, int ms) throws JSONException {
-        tempRobotMatchData.getJSONArray("switchFriendly").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+    public static void addSwitchFriendly(String type, int ms){
+        try {
+            tempRobotMatchData.getJSONArray("switchFriendly").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addSwitchEnemy(String type, int ms) throws JSONException {
-        tempRobotMatchData.getJSONArray("switchEnemy").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+    public static void addSwitchEnemy(String type, int ms){
+        try {
+            tempRobotMatchData.getJSONArray("switchEnemy").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addVault(String type, int ms) throws JSONException {
-        tempRobotMatchData.getJSONArray("vault").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+    public static void addVault(String type, int ms){
+        try {
+            tempRobotMatchData.getJSONArray("vault").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void addPortal(String type, int ms) throws JSONException {
-        tempRobotMatchData.getJSONArray("portal").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+    public static void addPortal(String type, int ms){
+        try {
+            tempRobotMatchData.getJSONArray("portal").put(new JSONObject("{\"type\":\""+ type +"\",\"time\":"+ms+"}"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void removeScale() throws JSONException {
-        tempRobotMatchData.getJSONArray("scale").remove(tempRobotMatchData.getJSONArray("scale").length()-1);
+    public static void removeScale() {
+        try {
+            tempRobotMatchData.getJSONArray("scale").remove(tempRobotMatchData.getJSONArray("scale").length()-1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void removeSwitchFriendly() throws JSONException {
-        tempRobotMatchData.getJSONArray("switchFriendly").remove(tempRobotMatchData.getJSONArray("switchFriendly").length()-1);
+    public static void removeSwitchFriendly() {
+        try {
+            tempRobotMatchData.getJSONArray("switchFriendly").remove(tempRobotMatchData.getJSONArray("switchFriendly").length()-1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void removeSwitchEnemy() throws JSONException {
-        tempRobotMatchData.getJSONArray("switchEnemy").remove(tempRobotMatchData.getJSONArray("switchEnemy").length()-1);
+    public static void removeSwitchEnemy() {
+        try {
+            tempRobotMatchData.getJSONArray("switchEnemy").remove(tempRobotMatchData.getJSONArray("switchEnemy").length()-1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void removeVault() throws JSONException {
-        tempRobotMatchData.getJSONArray("vault").remove(tempRobotMatchData.getJSONArray("vault").length()-1);
+    public static void removeVault() {
+        try {
+            tempRobotMatchData.getJSONArray("vault").remove(tempRobotMatchData.getJSONArray("vault").length()-1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void removePortal() throws JSONException {
-        tempRobotMatchData.getJSONArray("portal").remove(tempRobotMatchData.getJSONArray("portal").length()-1);
+    public static void removePortal() {
+        try {
+            tempRobotMatchData.getJSONArray("portal").remove(tempRobotMatchData.getJSONArray("portal").length()-1);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void finishMatch(){
