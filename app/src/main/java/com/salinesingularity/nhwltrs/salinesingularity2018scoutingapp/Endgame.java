d0c1e53ea2910dd6c49d0f362dbcfe66e8316f1f
nhwltrs <nhwltrs>
2018-02-16 19:15:49 -0500

    Removed the innacurate data checkbox

--- app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/Endgame.java
+++ app/src/main/java/com/salinesingularity/nhwltrs/salinesingularity2018scoutingapp/Endgame.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Spinner;
